22fdd1bf9d1d333640bf7944d1587202063291e7
KSJ_SINGH <kunwardeepjagdev@gmail.com>
2022-03-15 03:25:52 +0530

    second commit

--- Covid Data exploration.sql
+++ Covid Data exploration.sql
@@ -1,69 +1,128 @@
 /*
 Covid 19 Data Exploration 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: JOINs, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
-/* Initialising the data*/
+/* InitialISing the data*/
 
-Select * from deaths
-where continent is not null
-order by 3,4;
+SELECT * FROM deaths
+WHERE continent IS NOT NULL
+ORDER BY 3,4;
 
-/*Selecting the data we will be needing*/
+/*SELECTing the data we will be needing*/
 
-select location, date, population, total_cases, total_deaths
-from deaths
-where continent is not null
-order by 1,2;
+SELECT location, date, population, total_cases, total_deaths
+FROM deaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2;
 
 -- Total cases vs Total deaths
 -- Likelihood of dying if you contract covid in your country of residence
 
-select location, date, population,total_cases, total_deaths, round((total_deaths/total_cases)*100,2) as Deathpercentage
-from deaths
-where location = 'India';
+SELECT location, date, population,total_cases, total_deaths, ROUND((total_deaths/total_cases)*100,2) as Deathpercentage
+FROM deaths
+WHERE location = 'India' and total_cases IS NOT NULL and total_deaths IS NOT NULL;
 
 --Total Cases vs Population
 --Illustrates the proportion of population infected with the Virus
 
-select location, date, population,total_cases,round((total_cases/population)*100,2) as InfectionRate
-from deaths
-where location = 'India'
-order by 2 asc;
+SELECT location, date, population,total_cases,ROUND((total_cases/population)*100,2) as InfectionRate
+FROM deaths
+WHERE location = 'India'
+ORDER BY 2 asc;
 
 
 --Total Deaths vs Population
 --Illustrates the probability of dying during the pandemic
 
-select location, date, population, total_deaths, round((total_deaths/population)*100, 2) as DeathPercentage
-from deaths
-where location = 'India'
-order by 2 asc;
+SELECT location, date, population, total_deaths, ROUND((total_deaths/population)*100, 2) as DeathPercentage
+FROM deaths
+WHERE location = 'India'
+ORDER BY 2 asc;
 
 
 --Countries with the highest infection rate compared to Population
-select location, max(total_cases) as HighestInfectionCount, max(round((total_cases/population)*100,2))as HighestInfectionPercentage
-from deaths
-where  total_cases is not null
-group by location
-order by HighestInfectionCount desc;
+SELECT location,SUM(total_cases) as HighestInfectionCount,SUM(ROUND((total_cases/population)*100,2))as HighestInfectionPercentage
+FROM deaths
+WHERE  total_cases IS NOT NULL
+GROUP BY location
+ORDER BY HighestInfectionCount desc;
 
 
 -- Countries with Highest Death Count per Population
 
-select location, max(total_deaths) as HighestDeathCount, max(round((total_deaths/population)*100, 2)) as HighestDeathPercentage
-from deaths
-where total_deaths is not null and population is not null
-group by location
-order by HighestDeathCount desc;
+SELECT location,SUM(total_deaths) as HighestDeathCount,SUM(ROUND((total_deaths/population)*100, 2)) as HighestDeathPercentage
+FROM deaths
+WHERE total_deaths IS NOT NULL and population IS NOT NULL
+GROUP BY location
+ORDER BY HighestDeathCount desc;
 
 
--- BREAKING THINGS DOWN BY CONTINENT
 
 -- Showing contintents with the highest death count per population
 
-select continent, max(total_deaths)  as HighestDeathCount, max(round((total_deaths/population)*100, 2)) as HighestDeathPercentage
-from deaths
-where continent is not null
-group by continent
-order by HighestDeathCount desc;
+SELECT continent,SUM(total_deaths)  as HighestDeathCount,SUM(ROUND((total_deaths/population)*100, 2)) as HighestDeathPercentage
+FROM deaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY HighestDeathCount desc;
+
+-- Showing continents with the highest infection rate compared to Population
+
+SELECT continent,SUM(total_cases) as HighestInfectionCount,SUM(ROUND((total_cases/population)*100,2))as HighestInfectionPercentage
+FROM deaths
+WHERE continent IS NOT NULL and total_cases IS NOT NULL
+GROUP BY continent
+ORDER BY HighestInfectionCount desc;
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine in India
+ 
+ SELECT d.location, d.date,d.population,  v.new_vaccinations,  ROUND((v.new_vaccinations/d.population)*100,2) as VaccinationPercentage
+ FROM deaths d
+ JOIN vaccinations v on d.date = v.date and d.location = v.location
+ WHERE d.continent IS NOT NULL and v.new_vaccinations IS NOT NULL and d.location = 'india'
+ ORDER BY d.date asc;
+
+ --Total Population vs fully vaccinated
+ --Shows Percentage of Population that have been fully vacinnatied in India
+
+ SELECT d.location, d.date, d.population, v.people_fully_vaccinated,ROUND((v.people_fully_vaccinated/population)*100,2) as FullyVaccinatedPercentage
+ FROM deaths d
+ JOIN vaccinations v on d.date = v.date and d.location = v.location
+ WHERE d.continent IS NOT NULL and v.people_fully_vaccinated IS NOT NULL and d.location = 'india'
+ ORDER BY d.date asc;
+
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+
+Select d.continent, d.location, d.date, d.population, v.new_vaccinations
+, SUM(CONVERT(int,v.new_vaccinations)) OVER (Partition by d.Location Order by d.location, d.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From deaths as d
+Join vaccinations as v
+	On d.location = v.location
+	and d.date = v.date
+where d.continent is not null 
+order by 2,3
+
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select d.continent, d.location, d.date, d.population, v.new_vaccinations
+, SUM(CONVERT(int,v.new_vaccinations)) OVER (Partition by d.Location Order by d.location, d.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From deaths as d
+Join vaccinations as v
+	On d.location = v.location
+	and d.date = v.date
+where d.continent is not null 
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100
+From PopvsVac
